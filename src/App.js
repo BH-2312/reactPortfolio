f24bc8cf145a411879013b0127bc9a161c97d455
Ben Hilliard <benhilliard23@hotmail.com>
2020-10-03 18:56:36 +0930

    basic structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Body from "./components/body";
+
+function App() {
+  return (
+    <div className="App">
+      <Body>
+
+        
+      </Body>
+    </div>
+  );
+}
+
+export default App;
