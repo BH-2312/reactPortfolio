08fce27da3d4315a88c99bba64b90e490f6b6327
Ben Hilliard <benhilliard23@hotmail.com>
2020-10-23 18:03:46 +1030

    Updates

---
+++ precache-manifest.488bc8de66e65154cecd3d4447a0426f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613eea8477641435c55a9b1a9c58e442",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "f9de13c0c2748833bf4a",
+    "url": "/reactPortfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bb014619467da1cfd664",
+    "url": "/reactPortfolio/static/css/main.37bc0feb.chunk.css"
+  },
+  {
+    "revision": "f9de13c0c2748833bf4a",
+    "url": "/reactPortfolio/static/js/2.a0ef295e.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/reactPortfolio/static/js/2.a0ef295e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb014619467da1cfd664",
+    "url": "/reactPortfolio/static/js/main.b487e1d3.chunk.js"
+  },
+  {
+    "revision": "beded403309fe67800d5",
+    "url": "/reactPortfolio/static/js/runtime-main.8278393b.js"
+  },
+  {
+    "revision": "814006f19179c66a8d371dcbd220daeb",
+    "url": "/reactPortfolio/static/media/CaptureWorkScheduler.814006f1.png"
+  },
+  {
+    "revision": "69d22c75c4d5c5816af484d4bec14801",
+    "url": "/reactPortfolio/static/media/LandingPage.69d22c75.png"
+  },
+  {
+    "revision": "f5080359511c5197644fdee6a4308c08",
+    "url": "/reactPortfolio/static/media/MusicQuizDemo.f5080359.gif"
+  },
+  {
+    "revision": "f5d10c806eb6f7c570e5024c7cd6f878",
+    "url": "/reactPortfolio/static/media/PasswordGeneratorSnapshot.f5d10c80.png"
+  }
+]);
